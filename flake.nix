4d37939405f91c5bb6a3e3d2b1e144c76b23ff0b
Kyu Han <kyu@insilica.co>
2025-05-29 14:09:52 -0400

    Update Python version from 310 to 3

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   outputs = { self, nixpkgs, ... }: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    pythonEnv = pkgs.python310.withPackages (ps: with ps; [
+    pythonEnv = pkgs.python3.withPackages (ps: with ps; [
       rdkit
       pandas
       rapidfuzz
