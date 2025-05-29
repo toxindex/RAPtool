a1b880538d66b4a8569ac1d0973cc56103736c69
Kyu Han <kyu@insilica.co>
2025-05-29 13:54:48 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,28 +3,32 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
-    outputs = { self, nixpkgs, ... }: {
-    devShells."x86_64-linux".default = nixpkgs.legacyPackages."x86_64-linux".mkShell {
-        buildInputs = with nixpkgs.legacyPackages."x86_64-linux".python310Packages; [
-        rdkit
-        pandas
-        rapidfuzz
-        aiohttp
-        tenacity
-        pyarrow
-        fastparquet
-        seaborn
-        matplotlib
-        scikit-learn
-        cython
-        blosc2
-        ];
-
-        shellHook = ''
+  outputs = { self, nixpkgs, ... }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    pythonEnv = pkgs.python310.withPackages (ps: with ps; [
+      rdkit
+      pandas
+      rapidfuzz
+      aiohttp
+      tenacity
+      pyarrow
+      fastparquet
+      seaborn
+      matplotlib
+      scikit-learn
+      cython
+      blosc2
+    ]);
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      buildInputs = [ pythonEnv ];
+      shellHook = ''
         echo "Installing pip-only packages..."
         python3 -m pip install --quiet --no-cache-dir pubchempy FuzzyTM fastcluster
-        '';
-        };
+      '';
     };
 
+    # This exposes the Python environment as a package for use in other flakes:
+    packages."x86_64-linux".default = pythonEnv;
+  };
 }
